3766bba08255016f24dfea4cf3ccab806cb17a8d
Alejandro Cruz <alexcruz_c96@outlook.com>
2025-06-03 13:24:02 -0600

    Decidí quitar la animación de la imagen y agregué información útil

--- UI_Designs/js/app.js
+++ UI_Designs/js/app.js
@@ -1,24 +1,14 @@
 //Variables
 
 const infoForm = document.querySelector('#infoContainer');
-const imageContainer = document.querySelector('#imageContainer');
 
 window.addEventListener('scroll', () =>{
     const rect = infoForm.getBoundingClientRect();
-    const rectImage = imageContainer.getBoundingClientRect();
     const windowHeight = window.innerHeight || document.documentElement.clientHeight;
     if (rect.top < windowHeight && rect.bottom > 0) {
         infoForm.classList.add('animate-fade-in-up');
     }
     else{
         infoForm.classList.remove('animate-fade-in-up');
-    }
-
-    const image = imageContainer.children[0].children[0].children[0];
-    if( rectImage.top < windowHeight && rectImage.bottom > 0) {
-        image.classList.add('dark');
-    }
-    else{
-        image.classList.remove('dark');
     }
 })
