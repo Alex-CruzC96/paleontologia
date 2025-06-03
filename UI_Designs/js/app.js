23571510074484c6d501f748d8130de43d028041
Alejandro Cruz <alexcruz_c96@outlook.com>
2025-06-03 15:16:29 -0600

    Se agregó el apartado del capibara

--- UI_Designs/js/app.js
+++ UI_Designs/js/app.js
@@ -2,10 +2,12 @@
 
 const infoForm = document.querySelector('#infoContainer');
 const glaciacionInfo = document.querySelector('#glaciacionesContainer');
+const capibaraContainer = document.querySelector('#capibaraContainer');
 
 window.addEventListener('scroll', () =>{
     const rect = infoForm.getBoundingClientRect();
     const rectGlaciacion = glaciacionInfo.getBoundingClientRect();
+    const rectCapibara = capibaraContainer.getBoundingClientRect();
     const windowHeight = window.innerHeight || document.documentElement.clientHeight;
     if (rect.top < windowHeight && rect.bottom > 0) {
         infoForm.classList.add('animate-fade-in-up');
@@ -19,5 +21,12 @@ window.addEventListener('scroll', () =>{
     }
     else{
         glaciacionInfo.classList.remove('dark');
+    }
+
+    if (rectCapibara.top < windowHeight && rectCapibara.bottom > 0) {
+        capibaraContainer.classList.add('animate-fade-in-up');
+    }
+    else{
+        capibaraContainer.classList.remove('animate-fade-in-up');
     }
 })
