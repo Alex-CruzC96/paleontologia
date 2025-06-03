19ab4a6e5951b6299ca20b68b507df49e4059dc5
Alejandro Cruz <alexcruz_c96@outlook.com>
2025-06-03 14:23:34 -0600

    Se agregó un apartado para las glaciaciones

--- UI_Designs/js/app.js
+++ UI_Designs/js/app.js
@@ -1,14 +1,23 @@
 //Variables
 
 const infoForm = document.querySelector('#infoContainer');
+const glaciacionInfo = document.querySelector('#glaciacionesContainer');
 
 window.addEventListener('scroll', () =>{
     const rect = infoForm.getBoundingClientRect();
+    const rectGlaciacion = glaciacionInfo.getBoundingClientRect();
     const windowHeight = window.innerHeight || document.documentElement.clientHeight;
     if (rect.top < windowHeight && rect.bottom > 0) {
         infoForm.classList.add('animate-fade-in-up');
     }
     else{
         infoForm.classList.remove('animate-fade-in-up');
+    }
+
+    if (rectGlaciacion.top < windowHeight && rectGlaciacion.bottom > 0) {
+        glaciacionInfo.classList.add('dark');
+    }
+    else{
+        glaciacionInfo.classList.remove('dark');
     }
 })
