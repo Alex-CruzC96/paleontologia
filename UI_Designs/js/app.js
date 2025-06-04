13b7108ab9066ac5bb814fa3074faa480bda7fbf
Alejandro Cruz <alexcruz_c96@outlook.com>
2025-06-04 09:05:02 -0600

    Optimicé el código JS para ser utilizado como clase

--- UI_Designs/js/app.js
+++ UI_Designs/js/app.js
@@ -1,42 +1,25 @@
-//Variables
+//Clase que llama a todos los elementos que se van a animar
+const animateElementsFirst = document.querySelectorAll('.animation-first');
+const animateElementsSecond = document.querySelectorAll('.animation-second');
 
-const infoForm = document.querySelector('#infoContainer');
-const glaciacionInfo = document.querySelector('#glaciacionesContainer');
-const capibaraContainer = document.querySelector('#capibaraContainer');
-const gliptodonteContainer = document.querySelector('#gliptodonteContainer');
-
-window.addEventListener('scroll', () =>{
-    const rect = infoForm.getBoundingClientRect();
-    const rectGlaciacion = glaciacionInfo.getBoundingClientRect();
-    const rectCapibara = capibaraContainer.getBoundingClientRect();
-    const rectGliptodonte = gliptodonteContainer.getBoundingClientRect();
-
+window.addEventListener('scroll',()=>{
     const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    if (rect.top < windowHeight && rect.bottom > 0) {
-        infoForm.classList.add('animate-fade-in-up');
-    }
-    else{
-        infoForm.classList.remove('animate-fade-in-up');
-    }
+    
+    animateElementsFirst.forEach((element) => {
+        const rect = element.getBoundingClientRect();
+        if (rect.top < windowHeight && rect.bottom > 0) {
+            element.classList.add('animate-fade-in-up');
+        } else {
+            element.classList.remove('animate-fade-in-up');
+        }
+    })
 
-    if (rectGlaciacion.top < windowHeight && rectGlaciacion.bottom > 0) {
-        glaciacionInfo.classList.add('dark');
-    }
-    else{
-        glaciacionInfo.classList.remove('dark');
-    }
-
-    if (rectCapibara.top < windowHeight && rectCapibara.bottom > 0) {
-        capibaraContainer.classList.add('animate-fade-in-up');
-    }
-    else{
-        capibaraContainer.classList.remove('animate-fade-in-up');
-    }
-
-    if (rectGliptodonte.top < windowHeight && rectGliptodonte.bottom > 0) {
-        gliptodonteContainer.classList.add('animate-fade-in-up');
-    }
-    else{
-        gliptodonteContainer.classList.remove('animate-fade-in-up');
-    }
+    animateElementsSecond.forEach((element) => {
+        const rect = element.getBoundingClientRect();
+        if (rect.top < windowHeight && rect.bottom > 0) {
+            element.classList.add('dark');
+        } else {
+            element.classList.remove('dark');
+        }
+    })
 })
