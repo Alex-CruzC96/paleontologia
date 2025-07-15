54f0f55216bcaee655ec1178983e75f510e63423
Alejandro Cruz <alexcruz_c96@outlook.com>
2025-07-15 13:39:32 -0600

    Se maquetó el mapa más bonito para las ubicaciones de los animalitos

--- docs/js/mapApp.js
+++ docs/js/mapApp.js
@@ -23,31 +23,37 @@ const generalMarkers = [
     {'location':[16.803190,-92.904648],'town_name':'Ixtapa'}
 ]
 
-//Marcadores de Venado cola blanca
+//Marcadores de artiodáctilos
 const markersVenados = [
     {'location': [16.759483,-93.105880],'town_name':'Tuxtla Gutierrez'}
+]
+
+const markersPecari = [
+    {'location':[16.733590,-92.636900],'town_name':'San Cristóbal de las Casas'}
+]
+
+const markersBisonte = [
+    {'location':[16.235115,-93.267251],'town_name':'Villa Flores'}
+]
+
+const markersCamello = [
+    {'location':[16.185110,-93.268307],'town_name':'Villa Corzo'}
 ]
 
 //Mapeo de los marcadores
 const markerMap = {
     generalMarkers,
-    markersVenados
+    markersVenados,
+    markersPecari,
+    markersBisonte,
+    markersCamello
 }
 
-//Eventos de click
-// markerChiapaCorzo.bindPopup("<b>Chiapa de Corzo</b><br>Aquí hay fósiles.");
-// markerVillaFlores.bindPopup("<b>VillaFlores</b><br>Aquí hay fósiles.");
-// markerVillaCorzo.bindPopup("<b>Villa Corzo</b><br>Aquí hay fósiles.");
-// markerLaConcordia.bindPopup("<b>La concordia</b><br>Aquí hay fósiles.");
-// markerSocoltenango.bindPopup("<b>Socoltenango</b><br>Aquí hay fósiles.");
-// markerSuchiapa.bindPopup("<b>Suchiapa</b><br>Aquí hay fósiles.");
-// markerComitan.bindPopup("<b>Comitán</b><br>Aquí hay fósiles.");
-// markerIxtapa.bindPopup("<b>Ixtapa</b><br>Aquí hay fósiles.");
+//Botones de especies evento de click
 
-//Botones de especies
 buttons.forEach(button => {
-    button.addEventListener('click',(b)=>{
-        let markerName = b.target.dataset.markers;
+    button.addEventListener('click',()=>{
+        let markerName = button.dataset.markers;
         let markers = markerMap[markerName];
         addMarkersToMap(markers);
     })
