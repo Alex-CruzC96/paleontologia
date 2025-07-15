2fe3a5277cc3967e3dbe690a56c757b8fc0e3f87
Alejandro Cruz <alexcruz_c96@outlook.com>
2025-07-15 11:24:35 -0600

    Se corrigió un error y se agregó un botón de acción para el index

--- docs/js/mapApp.js
+++ docs/js/mapApp.js
@@ -11,15 +11,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //Capa de marcadores dinámicos
 const markersLayer = L.layerGroup().addTo(map);
 
-//Marcadores
-// const markerChiapaCorzo = L.marker([16.708171,-93.016658]).addTo(markersLayer);
-// const markerVillaFlores = L.marker([16.235208,-93.267259]).addTo(markersLayer);
-// const markerVillaCorzo = L.marker([16.185083,-93.268279]).addTo(markersLayer);
-// const markerLaConcordia = L.marker([16.116043,-92.688880]).addTo(markersLayer);
-// const markerSocoltenango = L.marker([16.242937,-92.350305]).addTo(markersLayer);
-// const markerSuchiapa = L.marker([16.623211,-93.102707]).addTo(markersLayer);
-// const markerComitan = L.marker([16.252302,-92.135596]).addTo(markersLayer);
-// const markerIxtapa = L.marker([16.803190,-92.904648]).addTo(markersLayer);
+//Marcadores generales
+const generalMarkers = [
+    {'location':[16.708171,-93.016658],'town_name':'Chiapa de Corzo'},
+    {'location':[16.235208,-93.267259],'town_name':'Villa Flores'},
+    {'location':[16.185083,-93.268279],'town_name':'Villa Corzo'},
+    {'location':[16.116043,-92.688880],'town_name':'La Concordia'},
+    {'location':[16.242937,-92.350305],'town_name':'Socoltenango'},
+    {'location':[16.623211,-93.102707],'town_name':'Suchiapa'},
+    {'location':[16.252302,-92.135596],'town_name':'Comitán'},
+    {'location':[16.803190,-92.904648],'town_name':'Ixtapa'}
+]
 
 //Marcadores de Venado cola blanca
 const markersVenados = [
@@ -28,6 +30,7 @@ const markersVenados = [
 
 //Mapeo de los marcadores
 const markerMap = {
+    generalMarkers,
     markersVenados
 }
 
@@ -54,7 +57,7 @@ buttons.forEach(button => {
 function addMarkersToMap(markers){
     clearMarks(); // Limpia antes de agregar nuevos
     markers.forEach(marker => {
-        L.marker(marker.location).addTo(map).bindPopup(`<b>${marker.town_name}</b>`);
+        L.marker(marker.location).addTo(markersLayer).bindPopup(`<b>${marker.town_name}</b>`);
     })
 }
 
